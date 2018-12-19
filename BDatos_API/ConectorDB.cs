148d1868f1971d50b06c3bea338f70efa0094cda
jafetrd <jafetrd@gmail.com>
2018-12-19 14:44:47 -0600

    actualizacion

---
+++ BDatos_API/ConectorDB.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Alumnos
+{
+    class ConectorDB
+    {
+        public static SqlConnection ObtenerConexion()
+        {
+            SqlConnection conectar = new SqlConnection("server=127.0.0.1; database=egresadosie; Uid=root; pwd=0000000000;");
+            //MySqlConnection conectar = new MySqlConnection("server=127.0.0.1; database=egresadosie; Uid=root; pwd=;");
+            conectar.Open();
+            return conectar;
+        }//fin metodo estatico ObtenerConexion
+
+        public static SqlDataReader Consultas(string SQL)
+        {
+            /*Realizamos la consulta a la BD para vervicar los el usuario y su contraseña*/
+            SqlCommand comando = new SqlCommand(SQL, ObtenerConexion());
+
+            /*Ejecutamos la sentencia SQL descrita arriba*/
+            SqlDataReader reader = comando.ExecuteReader();
+
+            /*Retornamos lo que nos devolvio la consulta*/
+            return reader;
+        }//fin metodo estatico Consultas
+
+        public static int Inyectar(string SQL)
+        {
+            int respuesta = 0;
+            /*Realizamos la inserción a la BD para guardar lo que indique el SQL*/
+            SqlCommand comando = new SqlCommand(SQL, ObtenerConexion());
+
+            /*Ejecutamos la sentencia SQL descrita arriba*/
+            respuesta = comando.ExecuteNonQuery();
+
+            /*Retornamos un valor cualquiera*/
+            return respuesta;
+        }
+    }//fin de la clase
+}//fin del namespace
--- BDatos_API/MainWindow.xaml.cs
+++ BDatos_API/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
 
@@ -19,72 +11,107 @@ namespace BDatos_API
 {
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
+    /// Este código pertenece a la pantalla de Inicio de sesion ademas de tener metodos
+    /// que seran utilizados en otras partes del programa 
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
         List<Control> Controles = new List<Control>();
-
+        public static RoutedCommand comandoTecla = new RoutedCommand();
+        private bool _sePuedeEjecutar = true;
+        private bool _sePuedeEjecutar1 = true;
+        private bool boolcontrol = false;
         public MainWindow()
         {
             InitializeComponent();
             Controles.Add(caja_texto_usuario);  //0 caja de texto usuario
             Controles.Add(caja_contrasena);     //1 caja de contraseña
-            Controles.Add(boton_ingresar);      //2 boton ingresar
             Controles[0].Focus();
+            comandoTecla.InputGestures.Add(new KeyGesture(Key.I, ModifierKeys.Control));
         }
 
+
+
+        #region Botones y cajas de texto
+        //*******************Botones y cajas de texto********************************//
+        
+        //Cajas de texto de usuario y contraseña con enfoque al siguiente elemento
         private void Caja_texto_usuario_KeyDown(object sender, KeyEventArgs e)
         {
+            _sePuedeEjecutar = true;
             Marcar_control(Controles[0], true);
             if (e.Key == Key.Enter) Controles[1].Focus();
         }
 
         private void Caja_contrasena_KeyDown(object sender, KeyEventArgs e)
         {
+            _sePuedeEjecutar = true;
             Marcar_control(Controles[1], true);
-            if (e.Key == Key.Enter) Controles[2].Focus();
+            if (e.Key == Key.Enter) boton_ingresar.Focus();
         }
 
+        //Boton ingresar con Enter y con Click
         private void Boton_ingresar_Click(object sender, RoutedEventArgs e)
         {
-            Boton_ingresar_KeyDown(null, null);
+           if(boolcontrol==false) botoningresar();
         }
 
         private void Boton_ingresar_KeyDown(object sender, KeyEventArgs e)
         {
-            if (Seleccionar_control())
+            if (e.Key == Key.Enter & boolcontrol==false) botoningresar();
+        }
+
+        //Boton comandos rapidos con Enter y con Click
+        private async void Boton_comandos_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            _sePuedeEjecutar1 = false;
+            await botoncomandoAsync();
+        }
+
+        private async void Boton_comandos_KeyDownAsync(object sender, KeyEventArgs e)
+        {
+            _sePuedeEjecutar1 = false;
+            if(e.Key==Key.Enter) await botoncomandoAsync();
+        }
+
+        //Boton nueva cuenta con Enter y con Click
+        private void Boton_nueva_cuenta_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Boton_nueva_cuenta_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        #endregion
+
+        #region Metodos_de_operaciones
+        //*******************METODOS DE OPERACIONES*********************************//
+
+        private void botoningresar()
+        {
+            boolcontrol = Seleccionar_control();
+            if (boolcontrol)
             {
-                Ventana_principal ventana_Principal = new Ventana_principal();
-                ventana_Principal.Show();
-                this.Hide();
+                new Ventana_principal().Show();
+                this.Close();
             }
             else
             {
-                this.ShowMessageAsync("Inicio de sesión", "Datos incorrectos");
+                _sePuedeEjecutar = false;
+                Mensaje_asincrono("Inicio de sesión", "Datos incorrectos");
             }
         }
 
-        private void MetroWindow_KeyDown(object sender, KeyEventArgs e)
-        {
-            switch (e.Key)
-            {
-                case Key.LeftCtrl | Key.I:
-                    {
-                        Boton_ingresar_KeyDown(null, null);
-                    }
-                break;
-                case Key.LeftCtrl | Key.N:
-                    { }
-                break;
-                case Key.LeftCtrl | Key.H:
-                    { }
-                break;
-            }
-        }
+        //Recibe como parametro la forma a abrir y cierra la actual
+       
 
+        //Metodo para enfocar a un elemento que pertenezca a la clase Control
+        //si el campo esta vacio retorna falso y no permite las ejecucion siguiente
         private bool Seleccionar_control()
         {
-            //bool valor=true;
             foreach (Control a in Controles)
             {
                 switch (a.GetType().Name)
@@ -106,11 +133,13 @@ namespace BDatos_API
                             return false;
                         }
                         break;
+                    default: return true;
                 }
             }
             return true;
         }
-
+        
+        //Metodo para marcar las cajas vacias 
         private void Marcar_control(Control control,Boolean limpiar)
         {
             if (limpiar)
@@ -119,6 +148,33 @@ namespace BDatos_API
                 control.Background = Brushes.LightGray;
         }
 
-       
+        //Se muestra un mensaje al estilo METRO 
+        private void Mensaje_asincrono(string titulo, string mensaje)
+        {
+            this.ShowMessageAsync(titulo, mensaje);
+        }
+
+        //Muestra mensaje cuando boton comandos
+        private async System.Threading.Tasks.Task botoncomandoAsync()
+        {
+            var res = await this.ShowMessageAsync("Comandos", "Ctrl + I -> Iniciar sesión \nCtrl + N -> Nuevo usuario \nCtrl + H -> Ayuda",MessageDialogStyle.Affirmative);
+            if (res == MessageDialogResult.Affirmative)
+            {
+                _sePuedeEjecutar1 = true;
+            }
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _sePuedeEjecutar;
+            e.Handled = true;
+        }
+
+        private void CommandBinding_CanExecute_1(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _sePuedeEjecutar1;
+            e.Handled = true;
+        }
     }
+    #endregion
 }
